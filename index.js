8e0a787256d71faa238d9eea331e3b51555b6f4a
Yael Harosh <yaelharosh01@gmail.com>
2023-02-21 20:09:24 -0500

    pass all tests

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(arr) {
+    const result = [arr[0], arr[1]];
+    return result
+}
+// console.log(returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+const returnLastTwoDrivers = function(arr) {
+    const result = [];
+    result.push(arr[arr.length - 2], arr[arr.length - 1]);
+    return result;
+}
+// console.log(returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']));
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = num => (fare) => num * fare;
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arr, cb) => {
+    return cb(arr);
+}
+
+// console.log(selectDifferentDrivers(['Antonia', 'Nuru', 'Amari', 'Mo'], returnFirstTwoDrivers));
